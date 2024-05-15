d9d8ecf8d19de78398593a1e7021a6c586eb79fe
superninjaman5000 <96967841+superninjaman5000@users.noreply.github.com>
2024-05-14 21:18:47 -0700

    Band name Generator
    band name generator or just name genrator, could also be a password generator

---
+++ name generator.py
@@ -0,0 +1,9 @@
+print("welcome to the band name generator")
+
+city = input("what is the name of the city you grew up in?\n")
+
+pet = input("Wht is the name of your pet?\n")
+
+numbers = input("choose a number")
+
+print("Your name is", city+pet+numbers, "\n")
